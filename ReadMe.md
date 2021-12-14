48f20f7c9aa9908dfdef98681f747f01b965090b
matthaeus-k <ksh.common@gmail.com>
2021-12-14 11:22:47 +0900

    read me 수정

--- ReadMe.md
+++ ReadMe.md
@@ -1,15 +1,81 @@
 # 🚀 Welcome to your new awesome project!
 
+
+#삼성채용 프로젝트
+
+운영 시스템 정보
+```
+# OS    : rhel 7
+# WEB   : WebTob
+# WAS   : jeus8
+# DB    : oracle 19c 
+```
+
+개발 시스템 정보
+```
+# OS    : ubuntu
+# WEB   : Nginx
+# WAS   : tomcat 9
+# DB    : oracle 19c 
+```
+
+- App BackEnd
+```
+# Language        : Java8 jdk1.8.x
+# Project build   : Gradle
+# framework       : spring boot 2.5.7
+# dependencies    : build.gradle 확인 
+```
+- App FrontEnd
+```
+# view Template engines : freemarker
+# javascript 
+# webpack
+# eslint
+```
+- 배포
+```
+ war 풀소스 배포
+ 
+```
+
+
+
+- NodeJS Install
+
+```
+https://nodejs.org/ko/
+cmd> node -v 
+ v14.16.1
+cmd> npm -v 
+ 6.14.12
+
+```
 This project has been created using **webpack-cli**, you can now run
 
 ```
-npm run build
-```
-
-or
+ npm install --save-dev webpack
+ npm install --save-dev webpack-cli
 
 ```
-yarn build
+
+- eslint Install
+```
+npm install eslint --save-dev
+npm install eslint-webpack-plugin --save-dev
 ```
 
-to bundle your application
+-실행 명령어-
+```
+ cmd> esling --init // esling configuration
+ cmd> npm run dev:watch   
+ cmd> npm run lint  
+ 
+ * package.json 확인해주세요
+```
+
+
+
+
+
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,33 @@
 const path = require('path');
-const glob = require("glob");
+const glob = require('glob');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const target = process.env.npm_lifecycle_event;
 
 const filePath = {
-    src: path.join(__dirname, '/src/main/resources/static/scripts'),
-    build: path.join(__dirname, '/src/main/resources/static/public')
+  src: path.join(__dirname, '/src/main/resources/static/scripts'),
+  build: path.join(__dirname, '/src/main/resources/static/public'),
 };
 
-let entries = {} ;
-let strPath = filePath.src.split('\\').join('/')  ;
+const entries = {};
+const strPath = filePath.src.split('\\').join('/');
 
 glob.sync(filePath.src + '/**/*.js')
-    .filter(val => !val.includes('/modules/'))
-    .map(val => {
-        const name = val.replace(strPath, '');
-        entries[name.replace(/\.js$/, '')] = strPath + name;
+    .filter((val) => !val.includes('/modules/'))
+    .map((val) => {
+      const name = val.replace(strPath, '');
+      entries[name.replace(/\.js$/, '')] = strPath + name;
     });
 
 module.exports = {
-    entry: entries,
-    output: {
-        path: path.join(filePath.build,"modules") ,
-        filename: "[name].bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-            }
-        ]
-    }
-}
+  entry: entries,
+  output: {
+    path: path.join(filePath.build, 'modules'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+      new ESLintPlugin({
+        fix: true,
+        extensions: ['js']
+      })
+  ],
+};
